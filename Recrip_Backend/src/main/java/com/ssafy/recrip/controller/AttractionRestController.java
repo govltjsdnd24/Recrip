9b18620eaccf69e4c221212a2805c12a339d3d2a
govltjsdnd24 <gragasolaf@gmail.com>
2023-11-15 16:23:39 +0900

    랭크

--- Recrip_Backend/src/main/java/com/ssafy/recrip/controller/AttractionRestController.java
+++ Recrip_Backend/src/main/java/com/ssafy/recrip/controller/AttractionRestController.java
@@ -1,5 +1,6 @@
 package com.ssafy.recrip.controller;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,9 +10,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.recrip.model.GugunDto;
+import com.ssafy.recrip.model.RankDto;
 import com.ssafy.recrip.model.SidoDto;
 import com.ssafy.recrip.service.AttractionService;
 
@@ -68,6 +73,72 @@ public class AttractionRestController {
 		}
 		
 		ResponseEntity<Map<String,Object>> res= new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);
+		return res;
+	}
+	
+	@PostMapping("/restrankinsert")
+	public ResponseEntity<Map<String, Object>> restrankinsert(@RequestBody RankDto rankDto) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		try {
+			int result = service.insertRank(rankDto);
+			if(result != 0) {
+				map.put("resdata", "1");
+				map.put("resmsg", "랭크 등록 성공");
+			} else {
+				map.put("resdata", "0");
+				map.put("resmsg", "랭크 등록 실패");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			map.put("resmsg", "랭크 등록 오류 발생");
+		}
+		
+		ResponseEntity<Map<String, Object>> res = new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);
+		
+		return res;
+	}
+	
+	@GetMapping("/restranklist")
+	public ResponseEntity<Map<String,Object>> restranklist () {
+		Map<String,Object>map =new HashMap<>();
+		try {
+			List<RankDto>ranks= service.selectRank();
+			if(ranks.size()>0) {
+				map.put("resmsg", ranks);
+				map.put("resdata", "1");
+			} else {
+				map.put("resmsg", "랭크 리스트가 없음");
+				map.put("resdata", "0");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			map.put("resmsg", "랭크 리스트 불러오는 중 오류 발생");
+		}
+		
+		ResponseEntity<Map<String,Object>> res= new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);
+		return res;
+	}
+	
+	@PutMapping("/restrankupdate")
+	public ResponseEntity<Map<String, Object>> restrankupdate(@RequestBody RankDto rankDto) throws SQLException {
+		Map<String, Object> map = new HashMap<>();
+		try {
+			int result = service.updateRank(rankDto);
+			if(result != 0) {
+				map.put("resdata", "1");
+				map.put("resmsg", "랭크 수정 성공");
+			} else {
+				map.put("resdata", "0");
+				map.put("resmsg", "랭크 수정 실패");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			map.put("resmsg", "랭크 수정 오류 발생");
+		}
+		
+		ResponseEntity<Map<String, Object>> res = new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);
+		
 		return res;
 	}
 }
