ec9440b059f64e22ec2efb8d47e24d1003d46132
XrossD <sky500ro@gmail.com>
2023-11-16 10:23:02 +0900

    qweqw

--- Recrip_Frontend/src/main.js
+++ Recrip_Frontend/src/main.js
@@ -20,6 +20,8 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 
 const pinia = createPinia();
 const app = createApp(App);
@@ -28,5 +30,5 @@ pinia.use(piniaPluginPersistedstate);
 
 app.use(pinia);
 app.use(router);
-
+app.use(Antd);
 app.mount('#app');
